ff4b324717f8210ddb7a8e640dbd7316b956fb5e
CK Shie <sunvictor.n@gmail.com>
2018-05-18 22:34:29 +0900

    milestone version

--- detector.py
+++ detector.py
@@ -28,10 +28,19 @@ def overlapped_ratio(area_1, area_2):
     if overlapped_area > .0:
         overlapped_area_1 = overlapped_area/((area_1[2]-area_1[0])*(area_1[3]-area_1[1]))
         overlapped_area_2 = overlapped_area/((area_2[2]-area_2[0])*(area_2[3]-area_2[1]))
-        print('{:.4f}  {:.4f}'.format(overlapped_area_1, overlapped_area_2))
+        # print('{:.4f}  {:.4f}'.format(overlapped_area_1, overlapped_area_2))
         return max([overlapped_area_1, overlapped_area_2])
     else:
         return .0
+
+def read_file_list():
+    data_list = []
+    for dir in os.listdir(super_folder_path):
+        if os.path.isdir(os.path.join(super_folder_path, dir)):
+            data_list = data_list + \
+                    [os.path.join(super_folder_path, dir, f) for f in os.listdir(os.path.join(super_folder_path, dir))
+                     if f.endswith(('.jpg', 'jpeg','.png', '.bmp', '.JPG', 'JPEG','.PNG', '.BMP'))]
+    return data_list
 
 
 def main():
@@ -49,7 +58,7 @@ def main():
     categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
     category_index = label_map_util.create_category_index(categories)
 
-    TEST_IMAGE_PATHS = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(('.jpg', 'jpeg','.png', '.bmp', '.JPG', 'JPEG','.PNG', '.BMP'))]
+    TEST_IMAGE_PATHS = read_file_list()
     print('num of images = {}'.format(len(TEST_IMAGE_PATHS)))
 
     print('detecting...')
@@ -96,15 +105,30 @@ def main():
                 # save the info (one image one txt)
                 # label,ymin,xmin,ymax,xmax   (save as ratio)
                 if True:
-                    with open(PATH_TO_ANNOTATION, 'a+') as w:
-                        for bbox, c in zip(bbox_threshold, classes[0]):
-                            w.write('{},{},{},{},{}\n'.format(c, bbox[0], bbox[1], bbox[2], bbox[3]))
+                    with open(os.path.join(PATH_TO_ANNOTATION, os.path.basename(image_path) + '_.txt'), 'w') as w:
+                        for bbox, c in zip(thresholded_boxes, classes[0]):
+                            w.write('{},{},{},{},{}\n'.format(int(c), bbox[0], bbox[1], bbox[2], bbox[3]))
 
                 # move the data into single food, >2 food
+
                 if(len(thresholded_boxes)) > 1:
-                    shutil.move(image_path, os.path.join(folder_path, '2', os.path.basename(image_path)))
+                    if not os.path.exists(
+                            os.path.join(os.path.dirname(image_path), '2')):
+                        os.makedirs(
+                            os.path.join(os.path.dirname(image_path), '2'))
+                    dst_path = os.path.join(os.path.dirname(image_path), '2',
+                                            os.path.basename(image_path))
+                    shutil.move(image_path, dst_path)
+                    print(dst_path)
                 else:
-                    shutil.move(image_path, os.path.join(folder_path, '1', os.path.basename(image_path)))
+                    if not os.path.exists(
+                            os.path.join(os.path.dirname(image_path), '1')):
+                        os.makedirs(
+                            os.path.join(os.path.dirname(image_path), '1'))
+                    dst_path = os.path.join(os.path.dirname(image_path), '1',
+                                            os.path.basename(image_path))
+                    shutil.move(image_path, dst_path)
+                    print(dst_path)
 
                 
                 # Visualization of the results of a detection
